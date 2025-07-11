0926bdb880b3ba63eafb753f8e613eecdc8031f9
Programmering <teknisk+programmering-GDC2025-GR3@game.unf.dk>
2025-07-11 16:51:54 +0200

    instan

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class ShootingScript : MonoBehaviour
 {
+    public GameObject bullet;
+    public InputAction shoot;
+    public float cooldown = 0.5f;
+    float cooldownleft;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        shoot.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        cooldownleft -= Time.deltaTime;
+
+        if (shoot.WasPressedThisFrame() && cooldownleft <= 0)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            cooldownleft = cooldown;
+        }
     }
 }
