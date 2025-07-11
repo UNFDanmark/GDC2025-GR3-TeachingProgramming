f57f015d9847cac7fbe8a02fabf732a2cbd157fe
Programmering <teknisk+programmering-GDC2025-GR3@game.unf.dk>
2025-07-11 14:26:16 +0200

    del uno
    print statements

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("jeg bliver kaldt en enkelt gang");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("jeg bliver kaldt meget");
+    }
+}
+ 
