472ddc3abe09e8a8c277d2f7dfe82fe20db81bb9
Programmering <teknisk+programmering-GDC2025-GR3@game.unf.dk>
2025-07-12 11:00:47 +0200

    COINS!!
    coin rotation + bounce and spawn

--- Assets/Scripts/coinpickup.cs
+++ Assets/Scripts/coinpickup.cs
@@ -4,18 +4,20 @@ public class coinpickup : MonoBehaviour
 {
     private GameObject player22;
     private PlayerScript PS;
+    float EEE;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         PS = GameObject.Find("Player").GetComponent<PlayerScript>();
-        
+        EEE = Random.Range(0.9f,1.2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, 0f, -0.6f);
+        transform.position = new Vector3(transform.position.x,((Mathf.Sin(Time.time*EEE*3)+1)/7),transform.position.z);
     }
     
     private void OnTriggerEnter(Collider other)
--- Assets/Scripts/randomspawn.cs
+++ Assets/Scripts/randomspawn.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class randomspawn : MonoBehaviour
 {
+    public GameObject coin;
+    public float spawnCooldown = 2f;
+    float currentSpawnTimer;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,19 @@ public class randomspawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentSpawnTimer -= Time.deltaTime;
+
+        if (currentSpawnTimer <= 0)
+        {
+            Vector3 spawnPosition = transform.position;
+            spawnPosition.x = spawnPosition.x + Random.Range(-5, 5);
+            spawnPosition.z = spawnPosition.z + Random.Range(-5, 5);
+            spawnPosition.y = spawnPosition.y + 1;
+            
+            Instantiate(coin, spawnPosition, coin.transform.rotation);
+            currentSpawnTimer = spawnCooldown;
+        }
+
+
     }
 }
